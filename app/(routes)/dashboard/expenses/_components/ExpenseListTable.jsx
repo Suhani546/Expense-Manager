3166ccbd14bd4df00aed0b358b9a01c234c2b619
Suhani Jain <suhanijain551@gmail.com>
2024-07-06 13:21:48 +0530

    Update ExpenseListTable.jsx

--- app/(routes)/dashboard/expenses/_components/ExpenseListTable.jsx
+++ app/(routes)/dashboard/expenses/_components/ExpenseListTable.jsx
@@ -25,27 +25,35 @@ function ExpenseListTable({expenseList, refreshData}) {
 
   return (
     <div className='mt-3'>
-        <h2 className='font-bold text-lg mt-3 mb-6'>Latest Expense</h2>
-      <div className='grid grid-cols-4 bg-slate-200 p-2'>
-        <h2 className='font-bold'>Name</h2>
-        <h2 className='font-bold'>Amount</h2>
-        <h2 className='font-bold'>Date</h2>
-        <h2 className='font-bold'>Action</h2>
+    <h2 className='font-bold text-lg mt-3 mb-6'>Latest Expense</h2>
+    {expenseList.length > 0 ? (
+      <div>
+        <div className='grid grid-cols-4 bg-slate-200 p-2'>
+          <h2 className='font-bold'>Name</h2>
+          <h2 className='font-bold'>Amount</h2>
+          <h2 className='font-bold'>Date</h2>
+          <h2 className='font-bold'>Action</h2>
+        </div>
+        {expenseList.map((expenses, index) => (
+          <div key={index} className='grid grid-cols-4 bg-slate-50 p-2'>
+            <h2>{expenses.name}</h2>
+            <h2>{expenses.amount}</h2>
+            <h2>{expenses.createdAt ? moment(expenses.createdAt).format('DD/MM/YYYY') : ''}</h2>
+            <h2 className='ml-5'>
+              {expenses.id && (
+                <Trash
+                  className='text-red-600 cursor-pointer'
+                  onClick={() => deleteExpense(expenses)}
+                />
+              )}
+            </h2>
+          </div>
+        ))}
       </div>
-      {expenseList.map((expenses,index) => (
-        <div key={index} className='grid grid-cols-4 bg-slate-50 p-2'>
-        <h2>{expenses.name}</h2>
-        <h2>{expenses.amount}</h2>
-        <h2>{moment(expenses.createdAt).format('DD/MM/YYYY')}</h2>
-        <h2 className='ml-5'>
-            <Trash className='text-red-600 cursor-pointer'
-              onClick={() => deleteExpense(expenses)}
-            />
-        </h2>
-      </div>
-      ))}
-      
-    </div>
+    ) : (
+      <p>No expenses available.</p>
+    )}
+  </div>
   )
 }
 
